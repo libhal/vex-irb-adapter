8093442739bf51a1c1cdb47e5b8c0a2305d70d59
MaliaLabor <malia.labor@hotmail.com>
2025-09-05 12:08:11 -0700

    add vex code

---
+++ vex-code/vs-code-project/src/irb_adapter.cpp
@@ -0,0 +1,134 @@
+#include <cstddef>
+#include <cstdio>
+#include <iostream>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "irb_adapter.h"
+#include "vex.h"
+
+namespace irb_adapter {
+enum camera_index {
+  x_center_low,
+  x_center_hi,
+  y_center_low,
+  y_center_hi,
+  block_width_low,
+  block_width_hi,
+  block_height_low,
+  block_height_hi
+};
+
+enum photo_diode_index { diode_number, intensity_value };
+
+adapter::adapter(uint8_t p_port) : m_port(p_port) {
+  // make thread to run reads here
+  char port_buffer[12];
+  sprintf(port_buffer, "/dev/port%u", m_port);
+  m_port_file = fopen(port_buffer, "wb+");
+  if (m_port_file == NULL) {
+    printf("Failed opening port\n");
+  }
+}
+
+bool adapter::request_data(std::vector<char> &write_buffer,
+                           std::vector<char> &read_buffer) {
+  bool success = true;
+  if (m_port_file == NULL) {
+    printf("Port not open...\n");
+    return false;
+  }
+
+  auto bytes_written = fwrite(write_buffer.data(), sizeof write_buffer[0],
+                              write_buffer.size(), m_port_file);
+  if (bytes_written != write_buffer.size()) {
+    printf("Failed write to port, %u bytes written.\n", bytes_written);
+    success = false;
+  }
+  // wait for data
+  vex::this_thread::sleep_for(85);
+
+  // rudimentary "timeout"
+  size_t bytes_read = 0;
+  int attempts = 0;
+  while (attempts < 15 && bytes_read == 0) {
+    bytes_read = fread(&read_buffer[0], 1, read_buffer.size(), m_port_file);
+    attempts++;
+  }
+  if (bytes_read != read_buffer.size()) {
+    printf("Failed read from port, %u bytes read.\n", bytes_read);
+    success = false;
+  }
+  return success;
+}
+
+adapter::low_freq_data adapter::get_low_freq_data() {
+  std::vector<char> read_buffer(3);
+  std::vector<char> write_buffer = {'l'};
+  adapter::low_freq_data return_data;
+
+  if (request_data(write_buffer, read_buffer)) {
+    // check checksum
+    auto calculated_checksum =
+        read_buffer[diode_number] + read_buffer[intensity_value];
+    if (calculated_checksum != read_buffer.back()) {
+      printf("Checksum mismatch...\n");
+      return return_data;
+    }
+    return_data.low_freq_photo_diode = (int)read_buffer[diode_number];
+    return_data.low_freq_value = (int)read_buffer[intensity_value] & 0b01111111;
+  }
+  return return_data;
+}
+
+adapter::hi_freq_data adapter::get_hi_freq_data() {
+  std::vector<char> read_buffer(3);
+  std::vector<char> write_buffer = {'h'};
+  adapter::hi_freq_data return_data;
+
+  if (request_data(write_buffer, read_buffer)) {
+    // check checksum
+    auto calculated_checksum =
+        read_buffer[diode_number] + read_buffer[intensity_value];
+    if (calculated_checksum != read_buffer.back()) {
+      printf("Checksum mismatch...\n");
+      return return_data;
+    }
+    return_data.hi_freq_photo_diode = (int)read_buffer[diode_number];
+    return_data.hi_freq_value = (int)read_buffer[intensity_value] & 0b01111111;
+  }
+  return return_data;
+}
+
+adapter::camera_data adapter::get_cam_data() {
+  std::vector<char> read_buffer(9);
+  std::vector<char> write_buffer = {'c'};
+  adapter::camera_data return_data;
+
+  if (request_data(write_buffer, read_buffer)) {
+    // check checksum
+    uint8_t calculated_checksum = 0x00;
+    for (size_t i = 0; i < read_buffer.size() - 1; i++) {
+      calculated_checksum += read_buffer[i];
+    }
+    if (calculated_checksum != read_buffer.back()) {
+      printf("Checksum mismatch...\n");
+      return return_data;
+    }
+    return_data.x_center = ((uint8_t)read_buffer[x_center_hi] << 8) |
+                           (uint8_t)read_buffer[x_center_low];
+    return_data.y_center = ((uint8_t)read_buffer[y_center_hi] << 8) |
+                           (uint8_t)read_buffer[y_center_low];
+    return_data.block_width = ((uint8_t)read_buffer[block_width_hi] << 8) |
+                              (uint8_t)read_buffer[block_width_low];
+    return_data.block_height = ((uint8_t)read_buffer[block_height_hi] << 8) |
+                               (uint8_t)read_buffer[block_height_low];
+  }
+  return return_data;
+}
+
+} // namespace irb_adapter
---
+++ vex-code/vs-code-project/src/main.cpp
@@ -0,0 +1,125 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       malia                                                     */
+/*    Created:      6/7/2025, 10:47:12 AM                                     */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+#include <cstdio>
+
+#include "irb_adapter.h"
+#include "vex.h"
+
+using namespace vex;
+
+vex::brain Brain;
+vex::motor right_motor = motor(PORT20, ratio18_1, true);
+vex::motor left_motor = motor(PORT10, ratio18_1, false);
+vex::limit front_bumper = limit(Brain.ThreeWirePort.A);
+bool beacon_pressed = false;
+bool object_collected = false;
+
+void switch_pressed() {
+  if (!beacon_pressed) {
+    beacon_pressed = true;
+  } else {
+    object_collected = true;
+  }
+  // stop moving
+  right_motor.stop();
+  left_motor.stop();
+  this_thread::sleep_for(1000);
+  // back up for 1 sec
+  right_motor.spin(reverse, 40, rpm);
+  left_motor.spin(reverse, 40, rpm);
+  this_thread::sleep_for(1000);
+  // stop again
+  right_motor.stop();
+  left_motor.stop();
+  this_thread::sleep_for(1000);
+}
+
+void spin(bool clockwise, uint8_t power) {
+  if (clockwise) {
+    right_motor.spin(reverse, power, rpm);
+    left_motor.spin(forward, power, rpm);
+  } else {
+    right_motor.spin(forward, power, rpm);
+    left_motor.spin(reverse, power, rpm);
+  }
+}
+
+int main() {
+  irb_adapter::adapter board(1);
+  uint8_t threshold = 5;
+  uint8_t power = 40;
+  bool spin_clockwise = false;
+
+  front_bumper.pressed(switch_pressed);
+
+  while (1) {
+    Brain.Screen.clearScreen();
+    // check if bumper pressed
+    if (!beacon_pressed) {
+      auto low_freq_data = board.get_low_freq_data();
+      Brain.Screen.printAt(10, 20, "Low Frequency Beacon: PD %u: %u",
+                           low_freq_data.low_freq_photo_diode,
+                           low_freq_data.low_freq_value);
+      Brain.Screen.printAt(10, 40, "Camera Data: waiting...");
+      printf("LF %u:%u;\n", low_freq_data.low_freq_photo_diode,
+             low_freq_data.low_freq_value);
+
+      // spin in place if nothing detected
+      if (low_freq_data.low_freq_value < threshold) {
+        spin(spin_clockwise, power);
+      }
+      // turn clockwise if less than 3
+      else if (low_freq_data.low_freq_photo_diode < 3) {
+        spin_clockwise = true;
+        spin(spin_clockwise, power);
+      }
+      // turn counter clockwise if greater than 4
+      else if (low_freq_data.low_freq_photo_diode > 4) {
+        spin_clockwise = false;
+        spin(spin_clockwise, power);
+      }
+      // go straight if in middle
+      else {
+        right_motor.spin(forward, power, rpm);
+        left_motor.spin(forward, power, rpm);
+      }
+    } else if (beacon_pressed && !object_collected) {
+      auto cam_data = board.get_cam_data();
+      Brain.Screen.printAt(10, 20, "Low Frequency Beacon: Collected");
+      Brain.Screen.printAt(10, 40, "Camera Data: %d, %u (%u X %u)",
+                           cam_data.x_center, cam_data.y_center,
+                           cam_data.block_width, cam_data.block_height);
+      printf("Camera Data: X:%u Y:%u    %ux%u\n", cam_data.x_center,
+             cam_data.y_center, cam_data.block_width, cam_data.block_height);
+
+      // spin in place if nothing detected
+      if (cam_data.block_width == 0) {
+        spin(spin_clockwise, power);
+      }
+      // turn clockwise if x more than 155
+      else if (cam_data.x_center < 155) {
+        spin_clockwise = true;
+        spin(spin_clockwise, power);
+      }
+      // turn counter clockwise if x less than 165
+      else if (cam_data.x_center > 165) {
+        spin_clockwise = false;
+        spin(spin_clockwise, power);
+      }
+      // go straight if in middle
+      else {
+        right_motor.spin(forward, power, rpm);
+        left_motor.spin(forward, power, rpm);
+      }
+    } else {
+      Brain.Screen.printAt(10, 20, "Low Frequency Beacon: Collected");
+      Brain.Screen.printAt(10, 40, "Camera Data: Collected");
+    }
+  }
+}
